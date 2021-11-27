8da256e4f205acd8b7cd7e1ef71c6af8a709ace9
NatalliaK <natalliak.dev@gmail.com>
2021-11-27 01:30:53 -0800

    Updated aboutus page, started the carousel

--- Projects/MN Ballroom/js/scripts.js
+++ Projects/MN Ballroom/js/scripts.js
@@ -5,3 +5,18 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
