a9cf0f81c23186e08c3678204d0ec27fd2cff975
NatalliaK <natalliak.dev@gmail.com>
2021-11-27 05:17:50 -0800

    Added simple functionality for the book now button

--- Projects/MN Ballroom/js/scripts.js
+++ Projects/MN Ballroom/js/scripts.js
@@ -20,3 +20,12 @@ $(function() {
         }
     });
 });
+/*book now button*/
+$('div').on('click', function() {
+    $(this).toggleClass('show-description');
+});
+
+function book() {
+  const name = prompt('Hello, please tell us your name.');
+  const result = prompt(`Welcome to our website ${name}, please leave your email address or a phone number, and we will get back to you as soon as possible to schedule your first lesson!`);
+  }
